6496c466b98122550c6ada5ebd5aa9b97c1e43c0
Olga Strekalovskaia <strolse@yandex.ru>
2023-02-02 13:47:11 +0300

    added Excluding Mapbox GL JS explicitly from transpilation

--- src/components/Main/Map.jsx
+++ src/components/Main/Map.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
-import mapboxgl from 'mapbox-gl';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import mapboxgl from '!mapbox-gl';
 import './Map.css'
 import { drawRoute } from "../../asyncActions/api"
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3Ryb2xzZSIsImEiOiJjbDNkOG5nbWowMGQyM29sZnNqdW43ZTY5In0.FoQyIdGOq1qDGV7B6mxcjQ';
 function Map ({ isOrdered, coords }) {
     const map = useRef(null);
     const mapContainerRef = useRef(null);
 
     useEffect(() => {
+            mapboxgl.accessToken = 'pk.eyJ1Ijoic3Ryb2xzZSIsImEiOiJjbDNkOG5nbWowMGQyM29sZnNqdW43ZTY5In0.FoQyIdGOq1qDGV7B6mxcjQ';
             map.current = new mapboxgl.Map({
                 container: mapContainerRef.current,
                 style: 'mapbox://styles/mapbox/light-v10',
